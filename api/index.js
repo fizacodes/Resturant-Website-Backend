469a80b9e520b0f0acb99c4bcafe174a746deeb9
Fiza Shahid <fiza34037@gmail.com>
2026-05-23 10:46:28 +0500

    Setup Server Code

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import serverless from 'serverless-http';
+import connectdb from '../config/connectdb.js';
+import menuRoutes from '../routes/menuRoutes.js';
+import categoryRoutes from '../routes/categoryRoutes.js';
+import orderRoutes from '../routes/orderRoutes.js';
+import authroutes from '../routes/authroutes.js';
+import reservationRoutes from '../routes/reservationRoutes.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'https://resturant-website-frontend-pearl.vercel.app',
+    credentials: true,
+  })
+);
+
+connectdb();
+
+app.use('/api/auth', authroutes);
+app.use('/api/menu', menuRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/reservations', reservationRoutes);
+
+export default serverless(app);
--- config/connectdb.js
+++ config/connectdb.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 
 const connectdb = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }   }
-    export default connectdb
+  try {
+    if (mongoose.connection.readyState >= 1) {
+      return;
+    }
+
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    throw error;
+  }
+};
+
+export default connectdb;
